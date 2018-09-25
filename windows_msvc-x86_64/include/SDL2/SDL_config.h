27f239bd9048d0fbaa675d6baaed54bd7b1036f4
Joshua Duong <joshuaduong@google.com>
2018-09-25 07:33:17 -0700

    [windows_msvc] Add SDL_config.h generated header from build.
    Change-Id: I26947923d8cc5d92b7458bd974c54e5bf44f4856

--- windows_msvc-x86_64/include/SDL2/SDL_config.h
+++ windows_msvc-x86_64/include/SDL2/SDL_config.h
@@ -1,4 +1,3 @@
-/* include/SDL_config.h.  Generated from SDL_config.h.in by configure.  */
 /*
   Simple DirectMedia Layer
   Copyright (C) 1997-2014 Sam Lantinga <slouken@libsdl.org>
@@ -32,129 +31,124 @@
 /* General platform specific identifiers */
 #include "SDL_platform.h"
 
-/* Make sure that this isn't included by Visual C++ */
-#ifdef _MSC_VER
-#error You should run hg revert SDL_config.h 
-#endif
-
 /* C language features */
 /* #undef const */
 /* #undef inline */
 /* #undef volatile */
 
 /* C datatypes */
+/* Define SIZEOF_VOIDP for 64/32 architectures */
 #ifdef __LP64__
 #define SIZEOF_VOIDP 8
 #else
 #define SIZEOF_VOIDP 4
 #endif
-#define HAVE_GCC_ATOMICS 1
+
+/* #undef HAVE_GCC_ATOMICS */
 /* #undef HAVE_GCC_SYNC_LOCK_TEST_AND_SET */
 /* #undef HAVE_PTHREAD_SPINLOCK */
 
 /* Comment this if you want to build without any C library requirements */
-#define HAVE_LIBC 1
+/* #undef HAVE_LIBC */
 #if HAVE_LIBC
 
 /* Useful headers */
 /* #undef HAVE_ALLOCA_H */
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_STDIO_H 1
-#define STDC_HEADERS 1
-#define HAVE_STDLIB_H 1
+/* #undef HAVE_SYS_TYPES_H */
+/* #undef HAVE_STDIO_H */
+/* #undef STDC_HEADERS */
+/* #undef HAVE_STDLIB_H */
 #define HAVE_STDARG_H 1
-#define HAVE_MALLOC_H 1
-#define HAVE_MEMORY_H 1
-#define HAVE_STRING_H 1
-#define HAVE_STRINGS_H 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_CTYPE_H 1
-#define HAVE_MATH_H 1
+/* #undef HAVE_MALLOC_H */
+/* #undef HAVE_MEMORY_H */
+/* #undef HAVE_STRING_H */
+/* #undef HAVE_STRINGS_H */
+/* #undef HAVE_INTTYPES_H */
+/* #undef HAVE_STDINT_H */
+/* #undef HAVE_CTYPE_H */
+/* #undef HAVE_MATH_H */
 /* #undef HAVE_ICONV_H */
-#define HAVE_SIGNAL_H 1
+/* #undef HAVE_SIGNAL_H */
 /* #undef HAVE_ALTIVEC_H */
 /* #undef HAVE_PTHREAD_NP_H */
 /* #undef HAVE_LIBUDEV_H */
 /* #undef HAVE_DBUS_DBUS_H */
 
 /* C library functions */
-#define HAVE_MALLOC 1
-#define HAVE_CALLOC 1
-#define HAVE_REALLOC 1
-#define HAVE_FREE 1
+/* #undef HAVE_MALLOC */
+/* #undef HAVE_CALLOC */
+/* #undef HAVE_REALLOC */
+/* #undef HAVE_FREE */
 /* #undef HAVE_ALLOCA */
 #ifndef __WIN32__ /* Don't use C runtime versions of these on Windows */
-#define HAVE_GETENV 1
+/* #undef HAVE_GETENV */
 /* #undef HAVE_SETENV */
-#define HAVE_PUTENV 1
+/* #undef HAVE_PUTENV */
 /* #undef HAVE_UNSETENV */
 #endif
-#define HAVE_QSORT 1
-#define HAVE_ABS 1
+/* #undef HAVE_QSORT */
+/* #undef HAVE_ABS */
 /* #undef HAVE_BCOPY */
-#define HAVE_MEMSET 1
-#define HAVE_MEMCPY 1
-#define HAVE_MEMMOVE 1
-#define HAVE_MEMCMP 1
-#define HAVE_STRLEN 1
+/* #undef HAVE_MEMSET */
+/* #undef HAVE_MEMCPY */
+/* #undef HAVE_MEMMOVE */
+/* #undef HAVE_MEMCMP */
+/* #undef HAVE_STRLEN */
 /* #undef HAVE_STRLCPY */
 /* #undef HAVE_STRLCAT */
-#define HAVE_STRDUP 1
-#define HAVE__STRREV 1
-#define HAVE__STRUPR 1
-#define HAVE__STRLWR 1
+/* #undef HAVE_STRDUP */
+/* #undef HAVE__STRREV */
+/* #undef HAVE__STRUPR */
+/* #undef HAVE__STRLWR */
 /* #undef HAVE_INDEX */
 /* #undef HAVE_RINDEX */
-#define HAVE_STRCHR 1
-#define HAVE_STRRCHR 1
-#define HAVE_STRSTR 1
-#define HAVE_ITOA 1
-#define HAVE__LTOA 1
+/* #undef HAVE_STRCHR */
+/* #undef HAVE_STRRCHR */
+/* #undef HAVE_STRSTR */
+/* #undef HAVE_ITOA */
+/* #undef HAVE__LTOA */
 /* #undef HAVE__UITOA */
-#define HAVE__ULTOA 1
-#define HAVE_STRTOL 1
-#define HAVE_STRTOUL 1
-#define HAVE__I64TOA 1
-#define HAVE__UI64TOA 1
-#define HAVE_STRTOLL 1
-#define HAVE_STRTOULL 1
-#define HAVE_STRTOD 1
-#define HAVE_ATOI 1
-#define HAVE_ATOF 1
-#define HAVE_STRCMP 1
-#define HAVE_STRNCMP 1
-#define HAVE__STRICMP 1
-#define HAVE_STRCASECMP 1
-#define HAVE__STRNICMP 1
-#define HAVE_STRNCASECMP 1
-/* #undef HAVE_SSCANF */
-#define HAVE_VSSCANF 1
-/* #undef HAVE_SNPRINTF */
-#define HAVE_VSNPRINTF 1
-#define HAVE_M_PI /**/
-#define HAVE_ATAN 1
-#define HAVE_ATAN2 1
-#define HAVE_ACOS 1
-#define HAVE_ASIN 1
-#define HAVE_CEIL 1
-#define HAVE_COPYSIGN 1
-#define HAVE_COS 1
-#define HAVE_COSF 1
-#define HAVE_FABS 1
-#define HAVE_FLOOR 1
-#define HAVE_LOG 1
-#define HAVE_POW 1
-#define HAVE_SCALBN 1
-#define HAVE_SIN 1
-#define HAVE_SINF 1
-#define HAVE_SQRT 1
-#define HAVE_FSEEKO 1
-#define HAVE_FSEEKO64 1
+/* #undef HAVE__ULTOA */
+/* #undef HAVE_STRTOL */
+/* #undef HAVE_STRTOUL */
+/* #undef HAVE__I64TOA */
+/* #undef HAVE__UI64TOA */
+/* #undef HAVE_STRTOLL */
+/* #undef HAVE_STRTOULL */
+/* #undef HAVE_STRTOD */
+/* #undef HAVE_ATOI */
+/* #undef HAVE_ATOF */
+/* #undef HAVE_STRCMP */
+/* #undef HAVE_STRNCMP */
+/* #undef HAVE__STRICMP */
+/* #undef HAVE_STRCASECMP */
+/* #undef HAVE__STRNICMP */
+/* #undef HAVE_STRNCASECMP */
+/* #undef HAVE_VSSCANF */
+/* #undef HAVE_VSNPRINTF */
+/* #undef HAVE_M_PI */
+/* #undef HAVE_ATAN */
+/* #undef HAVE_ATAN2 */
+/* #undef HAVE_ACOS */
+/* #undef HAVE_ASIN */
+/* #undef HAVE_CEIL */
+/* #undef HAVE_COPYSIGN */
+/* #undef HAVE_COS */
+/* #undef HAVE_COSF */
+/* #undef HAVE_FABS */
+/* #undef HAVE_FLOOR */
+/* #undef HAVE_LOG */
+/* #undef HAVE_POW */
+/* #undef HAVE_SCALBN */
+/* #undef HAVE_SIN */
+/* #undef HAVE_SINF */
+/* #undef HAVE_SQRT */
+/* #undef HAVE_FSEEKO */
+/* #undef HAVE_FSEEKO64 */
 /* #undef HAVE_SIGACTION */
 /* #undef HAVE_SA_SIGACTION */
-#define HAVE_SETJMP 1
-#define HAVE_NANOSLEEP 1
+/* #undef HAVE_SETJMP */
+/* #undef HAVE_NANOSLEEP */
 /* #undef HAVE_SYSCONF */
 /* #undef HAVE_SYSCTLBYNAME */
 /* #undef HAVE_CLOCK_GETTIME */
@@ -164,11 +158,12 @@
 /* #undef HAVE_PTHREAD_SETNAME_NP */
 /* #undef HAVE_PTHREAD_SET_NAME_NP */
 /* #undef HAVE_SEM_TIMEDWAIT */
-
-#else
+#elif __WIN32__
 #define HAVE_STDARG_H 1
 #define HAVE_STDDEF_H 1
-#define HAVE_STDINT_H 1
+#else
+/* We may need some replacement for stdarg.h here */
+#include <stdarg.h>
 #endif /* HAVE_LIBC */
 
 /* SDL internal assertion support */
@@ -181,7 +176,7 @@
 /* #undef SDL_EVENTS_DISABLED */
 /* #undef SDL_FILE_DISABLED */
 /* #undef SDL_JOYSTICK_DISABLED */
-#define SDL_HAPTIC_DISABLED 1
+/* #undef SDL_HAPTIC_DISABLED */
 /* #undef SDL_LOADSO_DISABLED */
 /* #undef SDL_RENDER_DISABLED */
 /* #undef SDL_THREADS_DISABLED */
@@ -202,8 +197,8 @@
 /* #undef SDL_AUDIO_DRIVER_COREAUDIO */
 #define SDL_AUDIO_DRIVER_DISK 1
 #define SDL_AUDIO_DRIVER_DUMMY 1
-/* #undef SDL_AUDIO_DRIVER_XAUDIO2 */
-/* #undef SDL_AUDIO_DRIVER_DSOUND */
+#define SDL_AUDIO_DRIVER_XAUDIO2 1
+#define SDL_AUDIO_DRIVER_DSOUND 1
 /* #undef SDL_AUDIO_DRIVER_ESD */
 /* #undef SDL_AUDIO_DRIVER_ESD_DYNAMIC */
 /* #undef SDL_AUDIO_DRIVER_NAS */
@@ -224,17 +219,17 @@
 /* #undef SDL_INPUT_LINUXKD */
 /* #undef SDL_INPUT_TSLIB */
 /* #undef SDL_JOYSTICK_HAIKU */
-/* #undef SDL_JOYSTICK_DINPUT */
+#define SDL_JOYSTICK_DINPUT 1
 /* #undef SDL_JOYSTICK_DUMMY */
 /* #undef SDL_JOYSTICK_IOKIT */
 /* #undef SDL_JOYSTICK_LINUX */
-#define SDL_JOYSTICK_WINMM 1
+/* #undef SDL_JOYSTICK_WINMM */
 /* #undef SDL_JOYSTICK_USBHID */
 /* #undef SDL_JOYSTICK_USBHID_MACHINE_JOYSTICK_H */
 /* #undef SDL_HAPTIC_DUMMY */
 /* #undef SDL_HAPTIC_LINUX */
 /* #undef SDL_HAPTIC_IOKIT */
-/* #undef SDL_HAPTIC_DINPUT */
+#define SDL_HAPTIC_DINPUT 1
 
 /* Enable various shared object loading systems */
 /* #undef SDL_LOADSO_HAIKU */
@@ -254,6 +249,7 @@
 /* #undef SDL_TIMER_DUMMY */
 /* #undef SDL_TIMER_UNIX */
 #define SDL_TIMER_WINDOWS 1
+/* #undef SDL_TIMER_WINCE */
 
 /* Enable various video drivers */
 /* #undef SDL_VIDEO_DRIVER_HAIKU */
@@ -263,16 +259,20 @@
 #define SDL_VIDEO_DRIVER_DUMMY 1
 #define SDL_VIDEO_DRIVER_WINDOWS 1
 /* #undef SDL_VIDEO_DRIVER_WAYLAND */
-/* #undef SDL_VIDEO_DRIVER_WAYLAND_QT_TOUCH */
-/* #undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC */
-/* #undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC_EGL */
-/* #undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC_CURSOR */
-/* #undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC_XKBCOMMON */
+
+#if 0
+/* !!! FIXME: in configure script version, missing here: */
+#undef SDL_VIDEO_DRIVER_WAYLAND_QT_TOUCH
+#undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC
+#undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC_EGL
+#undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC_CURSOR
+#undef SDL_VIDEO_DRIVER_WAYLAND_DYNAMIC_XKBCOMMON
+#endif
+
 /* #undef SDL_VIDEO_DRIVER_MIR */
 /* #undef SDL_VIDEO_DRIVER_MIR_DYNAMIC */
 /* #undef SDL_VIDEO_DRIVER_MIR_DYNAMIC_XKBCOMMON */
 /* #undef SDL_VIDEO_DRIVER_X11 */
-/* #undef SDL_VIDEO_DRIVER_RPI */
 /* #undef SDL_VIDEO_DRIVER_X11_DYNAMIC */
 /* #undef SDL_VIDEO_DRIVER_X11_DYNAMIC_XEXT */
 /* #undef SDL_VIDEO_DRIVER_X11_DYNAMIC_XCURSOR */
@@ -294,8 +294,8 @@
 /* #undef SDL_VIDEO_DRIVER_X11_CONST_PARAM_XEXTADDDISPLAY */
 /* #undef SDL_VIDEO_DRIVER_X11_HAS_XKBKEYCODETOKEYSYM */
 
-/* #undef SDL_VIDEO_RENDER_D3D */
-/* #undef SDL_VIDEO_RENDER_D3D11 */
+#define SDL_VIDEO_RENDER_D3D 1
+#define SDL_VIDEO_RENDER_D3D11 1
 #define SDL_VIDEO_RENDER_OGL 1
 /* #undef SDL_VIDEO_RENDER_OGL_ES */
 /* #undef SDL_VIDEO_RENDER_OGL_ES2 */
@@ -307,9 +307,9 @@
 /* #undef SDL_VIDEO_OPENGL_ES2 */
 /* #undef SDL_VIDEO_OPENGL_BGL */
 /* #undef SDL_VIDEO_OPENGL_CGL */
-/* #undef SDL_VIDEO_OPENGL_EGL */
 /* #undef SDL_VIDEO_OPENGL_GLX */
 #define SDL_VIDEO_OPENGL_WGL 1
+/* #undef SDL_VIDEO_OPENGL_EGL */
 /* #undef SDL_VIDEO_OPENGL_OSMESA */
 /* #undef SDL_VIDEO_OPENGL_OSMESA_DYNAMIC */
 
@@ -330,5 +330,66 @@
 /* Enable assembly routines */
 #define SDL_ASSEMBLY_ROUTINES 1
 /* #undef SDL_ALTIVEC_BLITTERS */
+
+
+/* Platform specific definitions */
+#if !defined(__WIN32__)
+#  if !defined(_STDINT_H_) && !defined(_STDINT_H) && !defined(HAVE_STDINT_H) && !defined(_HAVE_STDINT_H)
+typedef unsigned int size_t;
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+typedef unsigned long uintptr_t;
+#  endif /* if (stdint.h isn't available) */
+#else /* __WIN32__ */
+#  if !defined(_STDINT_H_) && !defined(HAVE_STDINT_H) && !defined(_HAVE_STDINT_H)
+#    if defined(__GNUC__) || defined(__DMC__) || defined(__WATCOMC__)
+#define HAVE_STDINT_H	1
+#    elif defined(_MSC_VER)
+typedef signed __int8 int8_t;
+typedef unsigned __int8 uint8_t;
+typedef signed __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+typedef signed __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef signed __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#      ifndef _UINTPTR_T_DEFINED
+#        ifdef  _WIN64
+typedef unsigned __int64 uintptr_t;
+#          else
+typedef unsigned int uintptr_t;
+#        endif
+#define _UINTPTR_T_DEFINED
+#      endif
+/* Older Visual C++ headers don't have the Win64-compatible typedefs... */
+#      if ((_MSC_VER <= 1200) && (!defined(DWORD_PTR)))
+#define DWORD_PTR DWORD
+#      endif
+#      if ((_MSC_VER <= 1200) && (!defined(LONG_PTR)))
+#define LONG_PTR LONG
+#      endif
+#    else /* !__GNUC__ && !_MSC_VER */
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+#      ifndef _SIZE_T_DEFINED_
+#define _SIZE_T_DEFINED_
+typedef unsigned int size_t;
+#      endif
+typedef unsigned int uintptr_t;
+#    endif /* __GNUC__ || _MSC_VER */
+#  endif /* !_STDINT_H_ && !HAVE_STDINT_H */
+#endif /* __WIN32__ */
 
 #endif /* _SDL_config_h */
